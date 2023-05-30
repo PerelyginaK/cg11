97ae3e265bb803f1d7b26ae507a3d8fc0082771a
PerelyginaK <103136763+PerelyginaK@users.noreply.github.com>
2023-05-30 18:24:43 +0300

    Uraa

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.originalPictureBox = new System.Windows.Forms.PictureBox();
+            this.smoothedPictureBox = new System.Windows.Forms.PictureBox();
+            this.openButton = new System.Windows.Forms.Button();
+            this.smoothButton = new System.Windows.Forms.Button();
+            this.maskSizeNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.originalPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.smoothedPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.maskSizeNumericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // originalPictureBox
+            // 
+            this.originalPictureBox.Location = new System.Drawing.Point(110, 165);
+            this.originalPictureBox.Name = "originalPictureBox";
+            this.originalPictureBox.Size = new System.Drawing.Size(271, 225);
+            this.originalPictureBox.TabIndex = 0;
+            this.originalPictureBox.TabStop = false;
+            // 
+            // smoothedPictureBox
+            // 
+            this.smoothedPictureBox.Location = new System.Drawing.Point(406, 161);
+            this.smoothedPictureBox.Name = "smoothedPictureBox";
+            this.smoothedPictureBox.Size = new System.Drawing.Size(274, 228);
+            this.smoothedPictureBox.TabIndex = 1;
+            this.smoothedPictureBox.TabStop = false;
+            this.smoothedPictureBox.Click += new System.EventHandler(this.smoothedPictureBox_Click);
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(187, 65);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(112, 57);
+            this.openButton.TabIndex = 2;
+            this.openButton.Text = "button1";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // smoothButton
+            // 
+            this.smoothButton.Location = new System.Drawing.Point(445, 70);
+            this.smoothButton.Name = "smoothButton";
+            this.smoothButton.Size = new System.Drawing.Size(120, 51);
+            this.smoothButton.TabIndex = 3;
+            this.smoothButton.Text = "button1";
+            this.smoothButton.UseVisualStyleBackColor = true;
+            this.smoothButton.Click += new System.EventHandler(this.smoothButton_Click);
+            // 
+            // maskSizeNumericUpDown
+            // 
+            this.maskSizeNumericUpDown.Location = new System.Drawing.Point(619, 87);
+            this.maskSizeNumericUpDown.Name = "maskSizeNumericUpDown";
+            this.maskSizeNumericUpDown.Size = new System.Drawing.Size(81, 23);
+            this.maskSizeNumericUpDown.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.maskSizeNumericUpDown);
+            this.Controls.Add(this.smoothButton);
+            this.Controls.Add(this.openButton);
+            this.Controls.Add(this.smoothedPictureBox);
+            this.Controls.Add(this.originalPictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.originalPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.smoothedPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.maskSizeNumericUpDown)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox originalPictureBox;
+        private System.Windows.Forms.PictureBox smoothedPictureBox;
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.Button smoothButton;
+        private System.Windows.Forms.NumericUpDown maskSizeNumericUpDown;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        private Bitmap originalImage;
+        private Bitmap smoothedImage;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Filter = "Image Files (*.bmp, *.jpg, *.png)|*.bmp;*.jpg;*.png";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                originalImage = new Bitmap(openFileDialog.FileName);
+                originalPictureBox.Image = originalImage;
+                smoothedImage = new Bitmap(originalImage);
+                smoothedPictureBox.Image = smoothedImage;
+            }
+        }
+
+        private void smoothedPictureBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void smoothButton_Click(object sender, EventArgs e)
+        {
+            if (originalImage == null)
+            {
+                MessageBox.Show("Please open an image first.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            int maskSize = (int)maskSizeNumericUpDown.Value;
+            int[,] mask = new int[maskSize, maskSize];
+
+            for (int i = 0; i < maskSize; i++)
+            {
+                for (int j = 0; j < maskSize; j++)
+                {
+                    mask[i, j] = 1;
+                }
+            }
+
+            smoothedImage = ApplyMask(originalImage, mask);
+            smoothedPictureBox.Image = smoothedImage;
+        }
+
+        private Bitmap ApplyMask(Bitmap image, int[,] mask)
+        {
+            int maskSize = mask.GetLength(0);
+            int maskOffset = maskSize / 2;
+            Bitmap result = new Bitmap(image);
+
+            for (int i = maskOffset; i < image.Width - maskOffset; i++)
+            {
+                for (int j = maskOffset; j < image.Height - maskOffset; j++)
+                {
+                    int redSum = 0;
+                    int greenSum = 0;
+                    int blueSum = 0;
+
+                    for (int x = 0; x < maskSize; x++)
+                    {
+                        for (int y = 0; y < maskSize; y++)
+                        {
+                            Color pixelColor = image.GetPixel(i + x - maskOffset, j + y - maskOffset);
+                            redSum += pixelColor.R * mask[x, y];
+                            greenSum += pixelColor.G * mask[x, y];
+                            blueSum += pixelColor.B * mask[x, y];
+                        }
+                    }
+
+                    Color newColor = Color.FromArgb(
+                        Math.Min(255, redSum / maskSize / maskSize),
+                        Math.Min(255, greenSum / maskSize / maskSize),
+                        Math.Min(255, blueSum / maskSize / maskSize)
+                    );
+
+                    result.SetPixel(i, j, newColor);
+                }
+            }
+
+            return result;
+        }
+    }
+}
